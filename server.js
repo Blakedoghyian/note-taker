c327008a8f31c1a27941d011e8e660c940b0810d
Blakedoghyian <xblakedx@gmail.com>
2022-01-09 15:03:50 -0600

    add heroku and index path

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 // require express and instantiate server
 const express = require('express');
 const app = express();
+const path = require('path');
+
+// use heroku served port
+const PORT = process.env.PORT || 3001;
+
+
+// route to serve index.html page
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
 
 // listen for requests
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
